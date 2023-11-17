3f7b671928e830f432c462433fb43d9c079b5f43
Aidan Dionisio <aidanbcd@mac.com>
2023-11-16 21:39:33 -0800

    completed bonus example

--- Week 11+ - Pygame/example+.py
+++ Week 11+ - Pygame/example+.py
@@ -1,43 +1,73 @@
 # Example file showing a circle moving on screen
-# Modified slightly from https://www.pygame.org/docs/
+# Modified from https://www.pygame.org/docs/
 import pygame
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
-running = True
+dt = 0.0
 
+"""
+For more modularity, we separate the size of our player (circle radius)
+and how much we move the player each frame into their own variables.
+
+With this change, you could also change the size or step amount during runtime,
+like we do with position.
+"""
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_radius = 40
+movement_step = 300
 
+
+# This function replaces our logic from before, and adds some extra functionality
+## to stop us from moving beyond the screen. Read the given documentation to learn more.
+# If you make helper functions, remember to document them well so that you
+## (and your groupmates) know what you've written!
+def move_player(keystrokes):
+    """Reads the current keystrokes and moves the player by the amount
+    of pixels specified by the movement_step attribute.
+
+    Also checks the boundaries of the screen to make sure that the player never moves
+    outside them.
+
+    Args:
+        keystrokes: The sequence of key states from pygame.key.get_pressed()
+    """
+    if keystrokes[pygame.K_w] and (player_pos.y - player_radius) > 0:
+        player_pos.y -= movement_step * dt
+    if keystrokes[pygame.K_s] and (player_pos.y + player_radius) < screen.get_height():
+        player_pos.y += movement_step * dt
+    if keystrokes[pygame.K_a] and (player_pos.x - player_radius) > 0:
+        player_pos.x -= movement_step * dt
+    if keystrokes[pygame.K_d] and (player_pos.x + player_radius) < screen.get_width():
+        player_pos.x += movement_step * dt
+
+
+running = True
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
+    pygame.draw.circle(screen, "yellow", player_pos, player_radius)
 
-    pygame.draw.circle(screen, "yellow", player_pos, 40)
+    # Remember, we moved all the functionality from the loop to a function to make it
+    ## more readable!
+    move_player(pygame.key.get_pressed())
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 10
-    if keys[pygame.K_s]:
-        player_pos.y += 10
-    if keys[pygame.K_a]:
-        player_pos.x -= 10
-    if keys[pygame.K_d]:
-        player_pos.x += 10
-
-    # flip() the display to put your work on screen
     pygame.display.flip()
 
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    clock.tick(60)
+    """
+    "dt" is short for delta time: the number of seconds elapsed since
+    the previous frame.
+    
+    In game development, it's used to make physics that don't depend on
+    the game's framerate.
+    
+    You can see how it's used in move_player().
+    """
+    dt = clock.tick(60) / 1000
 
 pygame.quit()
