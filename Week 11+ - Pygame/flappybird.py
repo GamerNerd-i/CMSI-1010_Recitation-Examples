8763e3cd937098aeb67f69844c0b76387cbb2c02
Aidan Dionisio <aidanbcd@mac.com>
2023-11-17 13:35:39 -0800

    pipes correctly drawn and animated

--- Week 11+ - Pygame/flappybird.py
+++ Week 11+ - Pygame/flappybird.py
@@ -1,3 +1,5 @@
+from shutil import move
+import pip
 import pygame
 import pygame.rect as rect
 import pygame.display as Display
@@ -5,13 +7,10 @@ import pygame.draw as Draw
 import random
 
 GRAVITY = 0.2
-GAME_SPEED = 50
+GAME_SPEED = 2
 
 MAX_PIPES = 4
-PIPE_WIDTH_SCALAR = 10
 PIPE_Y_BUFFER = 10
-
-BIRD_SIZE = 30
 
 pygame.init()
 screen = Display.set_mode((720, 1280))
@@ -20,66 +19,67 @@ clock = pygame.time.Clock()
 HEIGHT = screen.get_height()
 WIDTH = screen.get_width()
 
+BIRD_SIZE = WIDTH // 10
+BIRD_RADIUS = BIRD_SIZE // 2
+
 
 class Pipe:
     def __init__(self):
         gap_top = random.randint(int(HEIGHT * 0.25), int(HEIGHT * 0.75))
-        gap_height = gap_top + random.randint(int(BIRD_SIZE * 1.25), BIRD_SIZE * 2)
-        starting_x = WIDTH * 1.25
+        gap_bottom = gap_top + random.randint(BIRD_SIZE * 2, BIRD_SIZE * 4)
+        self.starting_x = WIDTH * 1.25
 
         self.gap = {
             "top": gap_top,
-            "bottom": gap_height + gap_top,
+            "bottom": gap_bottom,
         }
+        self.width = int(BIRD_SIZE * 1.25)
 
         self.top_rect = rect.Rect(
-            (starting_x, -PIPE_Y_BUFFER),
-            (WIDTH / PIPE_WIDTH_SCALAR, self.gap["top"] + PIPE_Y_BUFFER),
+            (self.starting_x, -PIPE_Y_BUFFER),
+            (self.width, self.gap["top"] + PIPE_Y_BUFFER),
         )
         self.bottom_rect = rect.Rect(
-            (starting_x, self.gap["bottom"]),
-            (WIDTH / PIPE_WIDTH_SCALAR, HEIGHT + PIPE_Y_BUFFER),
+            (self.starting_x, self.gap["bottom"]),
+            (self.width, HEIGHT + PIPE_Y_BUFFER),
         )
 
         self.hit = False
-        self.offscreen = False
         self.color = "green"
 
     def draw(self):
         Draw.rect(screen, self.color, self.top_rect)
         Draw.rect(screen, self.color, self.bottom_rect)
 
-    def move(self, amount=GAME_SPEED):
-        self.top_rect.move_ip(-amount, 0)
-        self.bottom_rect.move_ip(-amount, 0)
+    def move(self, amount=-GAME_SPEED):
+        self.top_rect.move_ip(amount, 0)
+        self.bottom_rect.move_ip(amount, 0)
 
-        self.offscreen = self.top_rect.x + self.top_rect.width < 0
+        if self.top_rect.x + self.top_rect.width < 0:
+            self.top_rect.x = self.starting_x
+            self.bottom_rect.x = self.starting_x
 
 
 bird_y = screen.get_height() / 2
 bird_x = screen.get_width() / 3
 
 pipes = []
+pipe_gap = 2.5
 
 for pipe_num in range(MAX_PIPES):
-    newPipe = Pipe()
-    newPipe.move((WIDTH / MAX_PIPES) * pipe_num)
-    pipes.append(newPipe)
+    new_pipe = Pipe()
+    new_pipe.move(new_pipe.width * pipe_gap * pipe_num)
+    pipes.append(new_pipe)
 
 
 def move_all():
     for pipe in pipes:
         pipe.move()
 
-    index = 0
-    while len(pipes) > MAX_PIPES:
-        if pipes[index].offscreen:
-            pipes.remove(index)
-        else:
-            index += 1
-
 
 def draw_all():
+    Draw.circle(screen, "blue", (bird_x, bird_y), BIRD_RADIUS)
+
     for pipe in pipes:
         pipe.draw()
 
@@ -93,6 +93,7 @@ while running:
     screen.fill("skyblue")
 
     move_all()
+    draw_all()
 
     Display.flip()
     clock.tick(60)
