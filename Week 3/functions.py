c8a213673ad359a6bd08d9e757d30e3c2e8c006b
Aidan Dionisio <aidanbcd@mac.com>
2024-01-26 13:58:45 -0800

    functions + scope

--- Week 3/functions.py
+++ Week 3/functions.py
@@ -1,18 +1,83 @@
-# Multiple Inputs / Parameters
+# Functions are blocks of code that you "pre-write" to use later.
+"""
+There are three main reasons why we use functions:
+1) Readability: If you're rewriting the same block of code multiple times,
+    the rest of your code might become difficult to parse. By putting that 
+    rewritten code in a function, you can condense it down to a simple phrase
+    like "calculate_mean." It's much easier to read that than it would be to
+    identify the formula for a mean every time.
+    
+2) Generalization: Functions make code reusable. For example, if I write a
+    "calculate mean" function, then I can use it whenever I need to, no matter
+    what inputs I have. Using the formula would require it to change every time.
+    
+3) Maintenance: With a function, all the necessary code is in one place, even
+    if it's used in multiple places in code. Without functions, if you had to debug
+    a block you used 5 times, you would need to make the same changes 5 times, rather
+    than just once. 
+"""
+
+
+# Here's the most basic form of a function.
+def function_name(parameter1, parameter2):
+    print(parameter1 + " is definitely a " + parameter2)
+
+
+# Note the most important parts:
+"""
+1) "def" keyword: This lets Python know that you're creating a function,
+    just like how "if" tells Python that you're writing a conditional.
+    
+2) Function name: Follows the same naming conventions as variables. Like variables,
+    (and people) you "call" functions to run by their name.
+    
+3) Parameters: Inside the parentheses are the names for your inputs. Think of it like
+    initiating variables you'll need to complete the task. Note that, when you call the
+    function, the inputs do not need to have the same name as the parameter names given
+    here; in fact, it's better if they don't so that you don't get confused as to which
+    is which. Technically you can have as many parameters as you want, but be reasonable.
+"""
+
+# If you want to get an output for the function, use the "return" keyword.
+"""
+Be VERY careful about this point! Again: If you want a function to output a value that
+the rest of the code can use, use the RETURN keyword.
+
+Print does not do the same thing as return. It's only useful to peek at what's
+happening inside the function.
+
+One more time: USE RETURN. Print shows you a value, but DOES NOT save it for later use.
+"""
+
+
+# Calculates the mean of a list of numbers.
+def calculate_mean(numbers):
+    return sum(numbers) / len(numbers)
+
+
+random_list = [9, 5, 1, 4, 1, 9]
+# Here's how you call a function. Remember: use the name, and give it the parameters
+## that it needs.
+avg1 = calculate_mean(random_list)
+print(avg1)
+# Alternatively, just give it a direct value: no extra variables needed.
+avg2 = calculate_mean([8, 10, 7, 2, 7, 1])
+print(avg2)
+
+
+# Lastly, here's a showcase of some other fun quirks of functions.
+## Multiple Inputs / Parameters
 def is_multiple(num, factor):
     print("Called is_multiple on " + str(num) + " and " + str(factor))
     return num % factor == 0
 
 
-# Notice how short this is!
-
-
-# Multiple Outputs
+## Multiple Outputs
 def double_and_halve(num):
     return num * 2, num / 2
 
 
-# Functions in Functions, and nested conditionals
+## Functions can be put inside functions. This gets kinda crazy later on.
 def is_kind_of_prime(num):
     if is_multiple(num, 2):
         print(False)
--- Week 3/scope.py
+++ Week 3/scope.py
@@ -1,10 +1,73 @@
+# Scope refers to what the program can "see" at a given point.
+"""
+Think of your program like a factory. What parts you can see on the workfloor
+depends on where you're standing, or at what point of the manufacturing you are
+currently overseeing.
+
+For example, say your factory makes cars. If you're in the part of the factory
+where the engine goes into the chassis, there's a good chance that you didn't see
+the chassis get built -- it was just given to you, already complete. If you're on
+the walkways above the workfloor, you can see everything at once. If you're in a
+conference room going over financial reports, presumably, you won't see anything
+of the factory at all -- just your financial reports!
+
+Your position in the factory -- what you can see -- is your scope. Your program has
+the same idea: some code takes place in different "rooms." In those rooms, you can only
+see what's in the room, including what you brought into it. Here are some examples.
+"""
+
+# Global code can be seen by everything. You'll usually hear this in terms of variables.
+# Any completely unindented code is considered global.
+
+from tokenize import triple_quoted
+
+
+word = "dog"
+
+
+def print_word():
+    print(word)
+
+
+print(word)
+print_word()
+
+# Functions each have their own "local" scope.
+"""
+Local variables can only be seen inside its own function. This means that once the
+function finishes, those local variables cease to exist. By default, any parameters
+of the function are local.
+
+Here, I demonstrate that, if a global and a local variable share a name, the local
+variable (the one with a smaller scope) takes precedence.
+"""
+
 value = 5
 
 
-def print_different_number(num):
-    num = num + 5
-    print(num)
+# Notice here that our parameter has the same name as a global variable.
+def print_different_number(value):
+    value = value + 5
+    print(value)
 
 
 print_different_number(value)
+# Even though we altered the local "value" in the function, the global "value"
+## remains unchanged.
 print(value)
+
+# This next example is meant to crash. Once a function is finished, all its local
+## variables cease to exist.
+
+
+def a_lot_of_math():
+    start = 1 + 3
+    triple_sum = start * 3
+    half_triple = triple_sum / 2
+
+
+# Even without running the code, Python should be getting very mad at you for trying
+## to access names that don't exist anymore.
+print(start)
+print(triple_sum)
+print(half_triple)
